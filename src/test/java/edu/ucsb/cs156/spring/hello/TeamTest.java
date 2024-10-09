3bf98fdaf23d585d8b059698d9594a200102131c
Oviya Seeniraj <oviya@ucsb.edu>
2024-10-09 16:23:34 -0700

    finished team tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,16 +12,47 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("f24-14");    
     }
 
     @Test
     public void getName_returns_correct_name() {
-       assert(team.getName().equals("test-team"));
+       assert(team.getName().equals("f24-14"));
     }
 
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=f24-14, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_test() {
+        assert(team.equals(team) == true);
+        assert(team.equals(null) == false);
+        Team testTeam = new Team("f24-14");
+        testTeam.addMember("Oviya");
+        testTeam.addMember("Riona");
+        testTeam.addMember("Jennifer");
+        testTeam.addMember("Aman");
+        testTeam.addMember("Kevin");
+        testTeam.addMember("Vala");
+        assert(team.equals(testTeam)); // true case
+
+        // F && T case
+        Team falseTeam = new Team("WRONGGGG");
+        falseTeam.addMember("Oviya");
+        falseTeam.addMember("Riona");
+        falseTeam.addMember("Jennifer");
+        falseTeam.addMember("Aman");
+        falseTeam.addMember("Kevin");
+        falseTeam.addMember("Vala");
+        assert(team.equals(testTeam) == false);
+
+        // T && F case
+        Team tfTeam = new Team("f24-14");
+        assert(team.equals(testTeam) == false);
+    }
 }
